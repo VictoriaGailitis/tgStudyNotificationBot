7f0e6fd8fffbae942b1c018cc7c2f036ff2d568a
VictoriaGailitis <105845684+VictoriaGailitis@users.noreply.github.com>
2023-05-22 10:58:42 +0300

    second commit

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import re
 import time
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+from apiclient.discovery import build
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from google_auth_oauthlib.flow import InstalledAppFlow
 import sqlite3
@@ -27,10 +28,10 @@ if not creds or not creds.valid:
 
 # Настройки Telegram бота
 TELEGRAM_TOKEN = '6289852678:AAF491uAPXA1O97JG0UxDLNqzOoQ7k5Zppg'
-telegram_bot = Updater(token=TELEGRAM_TOKEN, use_context=True).bot
+telegram_bot = Updater(token=TELEGRAM_TOKEN, use_context=True)
 
 # Создаем базу данных и таблицу для хранения адресатов
-conn = sqlite3.connect('emails.db')
+conn = sqlite3.connect('emails.db',check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS emails
                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,7 +39,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS emails
                   email TEXT)''')
 
 # Функция, которая ищет новые письма от указанного адресата
-def check_email_updates():
+def check_email_updates(update, context):
     service = build('gmail', 'v1', credentials=creds)
     user_profile = service.users().getProfile(userId='me').execute()
     user_email = user_profile['emailAddress']
@@ -78,55 +79,54 @@ def check_email_updates():
             if message_text:
                 # Здесь можно проверить содержание сообщения на наличие ключевых слов, чтобы не уведомлять о ненужных письмах
                 telegram_message = f'У вас новое сообщение от {sender}: {snippet}'
-                telegram_bot.send_message(chat_id=user_id, text=telegram_message)
+                context.bot.send_message(chat_id=update.message.chat_id, text=telegram_message)
 
 # Команда для добавления адресата
-def add_email_start(bot, update):
-    update.message.reply_text('Введите адрес электронной почты, от которого вы хотите получать уведомления:')
+def add_email_start(telegram_bot, update):
+    telegram_bot.message.reply_text('Введите адрес электронной почты, от которого вы хотите получать уведомления:')
     # Добавляем хэндлер для обработки ответа пользователя
     return 'add-email'
 
-def add_email(bot, update):
-    user_id = update.message.chat_id
-    email = update.message.text
+def add_email(telegram_bot, update):
+    user_id = telegram_bot.message.chat_id
+    email = telegram_bot.message.text
     cursor.execute('INSERT INTO emails (user_id, email) VALUES (?, ?)', (user_id, email))
     conn.commit()
-    update.message.reply_text(f'Адрес {email} успешно добавлен.')
+    telegram_bot.message.reply_text(f'Адрес {email} успешно добавлен.')
 
 # Добавляем хэндлер для обработки ответа пользователя
-updater.dispatcher.add_handler(MessageHandler(Filters.regex(r'[1]+@[\w.-]+.\w+'), add_email), group=1)
+telegram_bot.dispatcher.add_handler(MessageHandler(Filters.regex(r'[1]+@[\w.-]+.\w+'), add_email), group=1)
 
 # Создаем клавиатуру для удаления обработчика
 keyboard = ReplyKeyboardMarkup([["Отмена"]], resize_keyboard=True, one_time_keyboard=True)
 
 # Добавляем хэндлер для отмены добавления адресата
-def add_email_cancel(bot, update):
-    update.message.reply_text("Добавление адресата отменено.", reply_markup=ReplyKeyboardRemove())
+def add_email_cancel(telegram_bot, update):
+    telegram_bot.message.reply_text("Добавление адресата отменено.", reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
 # Создаем ConversationHandler для запуска процесса добавления адресата
 add_email_handler = ConversationHandler(entry_points=[CommandHandler('add_email', add_email_start)],
                                         states={"add-email": [MessageHandler(Filters.regex(r'^[\w\.-]+@[\w\.-]+\.\w+'), add_email)]},
                                         fallbacks=[MessageHandler(Filters.text(['Отмена']), add_email_cancel)],allow_reentry=True)
-updater.dispatcher.add_handler(add_email_handler)
+telegram_bot.dispatcher.add_handler(add_email_handler)
 
 # Команда для просмотра списка адресатов
-def view_emails(bot, update):
-    user_id = update.message.chat_id
-    emails = [row[1] for row in cursor.execute('SELECT email FROM emails WHERE user_id = ?', (user_id,))]
+def view_emails(telegram_bot, update):
+    user_id = telegram_bot.message.chat_id
+    emails = [row[0] for row in cursor.execute('SELECT email FROM emails WHERE user_id = ?', (user_id,))]
     if emails:
         message = 'Список адресатов:\n' + '\n'.join(emails)
     else:
         message = 'Список адресатов пуст.'
-    update.message.reply_text(message)
+    telegram_bot.message.reply_text(message)
 
-# Создание обработчика команды и запуск бота
-updater = Updater(TELEGRAM_TOKEN)
-updater.dispatcher.add_handler(CommandHandler('add_email', add_email, pass_args=True))
-updater.dispatcher.add_handler(CommandHandler('view_emails', view_emails))
-updater.start_polling()
+telegram_bot.dispatcher.add_handler(CommandHandler('add_email', add_email, pass_args=True))
+telegram_bot.dispatcher.add_handler(CommandHandler('view_emails', view_emails))
+telegram_bot.dispatcher.add_handler(MessageHandler(Filters.text, check_email_updates))
+telegram_bot.start_polling()
 
 # Запуск бесконечного цикла для проверки новых писем
-while True:
-    check_email_updates()
-    time.sleep(60)
+#while True:
+    #check_email_updates()
+    #time.sleep(60)
